1bd6b31c5c0d8d4ff96a6851e3ad685147453cb8
Alex Vihman <alexg657@gmail.com>
2020-09-19 01:29:20 +0300

    Updates

---
+++ precache-manifest.795dd49bca1989cc8921b7e58d84fb7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e085232b52cafb21a3b685ebe0d93e01",
+    "url": "/alexg657/chuckNorrisJokeGenerator/index.html"
+  },
+  {
+    "revision": "2e56abe035caf813100e",
+    "url": "/alexg657/chuckNorrisJokeGenerator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "36145b9d3eed7bd61247",
+    "url": "/alexg657/chuckNorrisJokeGenerator/static/css/main.bb9c82f1.chunk.css"
+  },
+  {
+    "revision": "2e56abe035caf813100e",
+    "url": "/alexg657/chuckNorrisJokeGenerator/static/js/2.d0472d00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alexg657/chuckNorrisJokeGenerator/static/js/2.d0472d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36145b9d3eed7bd61247",
+    "url": "/alexg657/chuckNorrisJokeGenerator/static/js/main.f40f0fe7.chunk.js"
+  },
+  {
+    "revision": "828601bb7b0c4e0bf38e",
+    "url": "/alexg657/chuckNorrisJokeGenerator/static/js/runtime-main.40a6a42a.js"
+  }
+]);
